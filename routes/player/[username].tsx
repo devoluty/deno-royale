05b40ce7454644bfde9176e79bdfb5beb9fd7b4f
devoluty <mariotqm1@gmail.com>
2023-02-18 23:52:05 -0600

    remove token

--- routes/player/[username].tsx
+++ routes/player/[username].tsx
@@ -4,8 +4,7 @@ import { User } from "../../types.d.ts";
 export const handler: Handlers<User | null> = {
   async GET(_, ctx) {
     const { username } = ctx.params;
-    const token =
-      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImU0MWQ5ZWEyLTEzNWYtNDkzYi1iMzUwLWU3ZDkzNWU0NjZiYiIsImlhdCI6MTY3Njc2MDk5Niwic3ViIjoiZGV2ZWxvcGVyLzQ3NDMxNjYwLTcwNWUtYTFkMS0zNTFmLWE2MjNkMGM0MWI5OSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxNzcuMjI2LjEzNy4xNjIiXSwidHlwZSI6ImNsaWVudCJ9XX0.f5VidIpMyDL5YbxkRi0dGnjch4yK9oF1rfXBssqEAm8VPQ8m-7G8ijwQgVAPlQ7BceQEe2SvviXUavjNQ4QXeA";
+    const token = "";
 
     const url = `https://api.clashroyale.com/v1/players/%23${username}`;
     const resp = await fetch(url, {
